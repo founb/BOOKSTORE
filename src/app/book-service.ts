0941ff3d33973a1bbbe35bc909d380bd08409541
founb <ffounekefofana@gmail.com>
2026-02-27 14:06:09 +0100

    primero

--- src/app/book-service.ts
+++ src/app/book-service.ts
@@ -1,18 +1,22 @@
-import { Injectable,inject } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
+
+
 export class BookService {
   private http = inject(HttpClient);
   private url = 'http://localhost/dwc/index.php';
-  getAllBook(){
+
+
+  getAllBook() {
     this.http.get(this.url)
   }
-  createBook(){}
-  updateBook(){}
-  deleteBook(){}
 
+  createBook() { }
+  updateBook() { }
+  deleteBook() { }
 
 }
